c1240d14199d276d08379d98b73a3dd7965e6e1d
TARAK <tarakaramtirumala@gmail.com>
2023-03-06 12:55:19 +0530

    changes

--- roboshop-setup/ex1/servers.tf
+++ roboshop-setup/ex1/servers.tf
@@ -1,4 +1,11 @@
-
+resource "aws_instance" "ec2" {
+  ami                    = "ami-0a017d8ceb274537d"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = ["sg-02811f5637016bdbd"]
+  tags = {
+    Name = "test"
+  }
+}
 resource "aws_instance" "frontend" {
   ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
