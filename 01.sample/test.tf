d46378f68bebf6541585ffc23f92409891e2274c
TARAK <tarakaramtirumala@gmail.com>
2023-03-06 12:03:50 +0530

    changes

--- 01.sample/test.tf
+++ 01.sample/test.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2" {
   ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-0fc69f59ac03ac238"]
+  vpc_security_group_ids = ["sg-02811f5637016bdbd"]
   tags = {
     Name = "test"
   }
