c9eb78d699860ade3ea8f3bd477011ef85201c79
TARAK <tarakaramtirumala@gmail.com>
2023-03-07 09:40:04 +0530

    instance creation

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,33 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = ["119044227534"]
+}
+
+
+resource "aws_instance" "instances" {
+  for_each               = var.instances
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
+  vpc_security_group_ids = ["sg-0f914773417860a28"]
+  tags = {
+    Name = each.value["name"]
+  }
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
+output "ec2" {
+  value = [for k, v in aws_instance.instances : "${k} - ${v.public_ip}"]
+}
