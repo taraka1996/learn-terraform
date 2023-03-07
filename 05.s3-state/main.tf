10c167b9967e0294e9f6f2a8e857a54d4e9ed966
TARAK <tarakaramtirumala@gmail.com>
2023-03-08 01:04:24 +0530

    changes

--- 05.s3-state/main.tf
+++ 05.s3-state/main.tf
@@ -7,9 +7,9 @@ terraform {
 }
 
 resource "aws_instance" "ec2" {
-  ami                    = ""
+  ami                    = "ami-0a017d8ceb274537d"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = [""]
+  vpc_security_group_ids = ["sg-030ddb8656270edd0"]
   tags = {
     Name = "demo"
   }
